9195b7e2f36f3f29ef73455fb2621081a275881f
Karl <pastranakd@gmail.com>
2022-02-26 10:23:45 +0800

    Third Commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <NavBar />
       <Routes>
-        <Route exact path = '/' element = {<Home/>} />
+        <Route exact path = '/pastrana-karl/my-portfolio' element = {<Home/>} />
       </Routes>
       <FootBar />
     </Router>
